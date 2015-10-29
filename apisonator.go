263b19aac21e9eb969ce7a5c8d16393c6af513b6
Joaquim Moreno <jmprusi@daedalus.local>
2015-10-29 17:33:01 +0100

    register & login

--- apisonator.go
+++ apisonator.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 
 	"github.com/jawher/mow.cli"
 )
+
+type loginResponse struct {
+	ID        int    `json:"id"`
+	Email     string `json:"email"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	APIKey    string `json:"api_key"`
+}
 
 var (
 	app = cli.App("apisonator", "")
@@ -16,6 +26,8 @@ var (
 func main() {
 
 	app.Command("register", "Register user to Apisonator.io", register)
+	app.Command("login", "Login to Apisonator.io", login)
+
 	app.Run(os.Args)
 
 }
@@ -35,14 +47,35 @@ func register(cmd *cli.Cmd) {
 			data := url.Values{}
 			data.Set("email", *email)
 			data.Add("password", *password)
-			resp, err := http.PostForm("http://api.apisonator.io/api/register", data)
+			resp, err := http.PostForm("http://api.apisonator.io/api/registrations.json", data)
 			if err != nil {
 				panic(err)
 			}
-			fmt.Println(resp)
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			if resp.StatusCode == http.StatusCreated {
+
+				// Extract this shit
+				var response loginResponse
+				if err := json.Unmarshal(body, &response); err != nil {
+					panic(err)
+				}
+				//Is always scary to remove things..
+				authFilePath := "/tmp/cacafuti"
+				authFilePath = os.Getenv("HOME") + "/.apisonator"
+				os.Remove(authFilePath)
+				authFile, _ := os.Create(authFilePath)
+				defer authFile.Close()
+				fmt.Fprintln(authFile, response.APIKey)
+
+				fmt.Println("Registered correctly. Logged!")
+
+			} else {
+				fmt.Println("Invalid Email")
+			}
 		}
 	}
-
 }
 
 func login(cmd *cli.Cmd) {
@@ -55,16 +88,38 @@ func login(cmd *cli.Cmd) {
 	)
 
 	cmd.Action = func() {
+
 		if *email != "" && *password != "" {
 			data := url.Values{}
 			data.Set("email", *email)
 			data.Add("password", *password)
-			resp, err := http.PostForm("http://api.apisonator.io/api/register", data)
+			resp, err := http.PostForm("http://api.apisonator.io/api/sessions.json", data)
 			if err != nil {
 				panic(err)
 			}
-			fmt.Println(resp)
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			if resp.StatusCode == http.StatusCreated {
+
+				// Extract this shit
+				var response loginResponse
+				if err := json.Unmarshal(body, &response); err != nil {
+					panic(err)
+				}
+				//Is always scary to remove things..
+				authFilePath := "/tmp/cacafuti"
+				authFilePath = os.Getenv("HOME") + "/.apisonator"
+				os.Remove(authFilePath)
+				authFile, _ := os.Create(authFilePath)
+				defer authFile.Close()
+				fmt.Fprintln(authFile, response.APIKey)
+
+				fmt.Println("Logged!")
+
+			} else {
+				fmt.Println("Invalid User/Password")
+			}
 		}
 	}
-
 }
