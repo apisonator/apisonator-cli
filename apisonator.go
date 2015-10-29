40a6c7c4be83bd683ba6284f6c677536b9e48354
Joaquim Moreno <jmprusi@daedalus.local>
2015-10-29 19:10:42 +0100

    Added Create

--- apisonator.go
+++ apisonator.go
@@ -8,6 +8,10 @@ import (
 	"net/url"
 	"os"
 
+	"github.com/ttacon/chalk"
+
+	"github.com/kyokomi/emoji"
+
 	"github.com/jawher/mow.cli"
 )
 
@@ -19,6 +23,15 @@ type loginResponse struct {
 	APIKey    string `json:"api_key"`
 }
 
+type createEndpoint struct {
+	ID        int    `json:"id"`
+	UserID    int    `json:"user_id"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	Endpoint  string `json:"endpoint"`
+	Subdomain string `json:"subdomain"`
+}
+
 var (
 	app = cli.App("apisonator", "")
 )
@@ -27,6 +40,7 @@ func main() {
 
 	app.Command("register", "Register user to Apisonator.io", register)
 	app.Command("login", "Login to Apisonator.io", login)
+	app.Command("create", "Create your apisonator endpoint", create)
 
 	app.Run(os.Args)
 
@@ -120,6 +134,46 @@ func login(cmd *cli.Cmd) {
 			} else {
 				fmt.Println("Invalid User/Password")
 			}
+		}
+	}
+}
+
+func create(cmd *cli.Cmd) {
+
+	cmd.Spec = "SUBDOMAIN ENDPOINT"
+
+	var (
+		name     = cmd.StringArg("SUBDOMAIN", "", "Name for your apisonator proxy $subdomain.apisonator.io")
+		endpoint = cmd.StringArg("ENDPOINT", "", "Your API endpoint")
+	)
+
+	cmd.Action = func() {
+		var apiKey string
+		authFilePath := os.Getenv("HOME") + "/.apisonator"
+		f, err := os.Open(authFilePath)
+		if err != nil {
+			fmt.Println("Error. Login first")
+			os.Exit(1)
+		}
+		fmt.Fscan(f, &apiKey)
+
+		data := url.Values{}
+		data.Set("subdomain", *name)
+		data.Add("endpoint", *endpoint)
+		data.Add("api_key", apiKey)
+
+		resp, _ := http.PostForm("http://api.apisonator.io/api/proxies.json", data)
+		body, _ := ioutil.ReadAll(resp.Body)
+		if resp.StatusCode == http.StatusCreated {
+			var response createEndpoint
+			if err := json.Unmarshal(body, &response); err != nil {
+				panic(err)
+			}
+			Success := emoji.Sprintf("\n:white_check_mark: Your apisonator endpoint: %shttp://%s.apisonator.io%s -> %s\n\n", chalk.Green, response.Subdomain, chalk.Reset, response.Endpoint)
+			fmt.Print(Success)
+		} else {
+			Failed := emoji.Sprintf("\n:red_circle: Subdomain %s does exists\n", *name)
+			fmt.Println(Failed)
 		}
 	}
 }
